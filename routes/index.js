6616671c75093c3766f61a47373280e730bf4c7f
Manoel Blue <manoelbluee@gmail.com>
2021-02-23 13:36:50 +0100

    add main book home route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,29 @@ var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
 
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  const books = await Book.findAll();
-  res.json(books);
+router.get('/', (req, res) => {
+  res.redirect('/books')
 });
+
+/* Books route */
+router.get('/books', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  res.render('index', {books});
+}));
+
+
 
 module.exports = router;
