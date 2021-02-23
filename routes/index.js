4d511ec3b71728853b4e5f796f13881dc4217b90
Manoel Blue <manoelbluee@gmail.com>
2021-02-23 14:32:38 +0100

    post new book route

--- routes/index.js
+++ routes/index.js
@@ -31,4 +31,20 @@ router.get('/books/new', (req, res) => {
   res.render('new-book');
 });
 
+/* POST create book. */
+router.post('/books/new', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    // res.redirect('/books/' + book.id);
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      // book = await Book.build(req.body);
+      res.render('error');
+    } else {
+      throw error;
+    }  
+  }
+}));
+
 module.exports = router;
