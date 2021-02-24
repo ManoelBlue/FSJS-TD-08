711948451f668386ccde7a357d0bab06db06e4cb
Manoel Blue <manoelbluee@gmail.com>
2021-02-24 21:35:17 +0100

    create search form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
+const { Op } = require("sequelize");
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -19,12 +20,43 @@ router.get('/', (req, res) => {
     res.redirect('/books')
 });
 
-/* Books route */
+/* GET Books route */
 router.get('/books', asyncHandler(async (req, res) => {
     const books = await Book.findAll();
     console.log(books.length);
     res.render('index', {books});
 }));
+
+/* POST books search route */
+router.post('/books', asyncHandler(async (req, res) => {
+    const query = req.body.query.toLowerCase();
+    const books = await Book.findAll({
+        where: {
+            [Op.or]: [
+                {
+                    title: {
+                    [Op.ilike]: `%${query}%`
+                    }
+                },
+                {
+                    author: {
+                    [Op.ilike]: `%${query}%`
+                    }
+                },
+                {
+                    genre: {
+                        [Op.ilike]: `%${query}%`
+                    }
+                },
+                {
+                    year: {
+                        [Op.ilike]: `%${query}%`
+                    }
+                },
+            ]
+        }
+    })
+}))
 
 /* GET create New Book route */
 router.get('/books/new', (req, res) => {
@@ -36,13 +68,14 @@ router.post('/books/new', asyncHandler(async (req, res) => {
     let book;
     try {
         book = await Book.create(req.body);
+        console.log(error);
         res.redirect('/books');
     } catch (error) {
         if(error.name === "SequelizeValidationError") {
             console.log(error);
             res.render('update-book', {
                 book,
-                errors: error.erros
+                error: error.erros
             });
         } else {
             throw error;
