7359f93e414f08da0cd7d1c1467820954f071a99
Manoel Blue <manoelbluee@gmail.com>
2021-02-26 17:04:39 +0100

    add pagination links

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
 const { Op } = require("sequelize");
+let limit = 5;
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -22,45 +23,45 @@ router.get('/', (req, res) => {
 
 /* GET Books route */
 router.get('/books', asyncHandler(async (req, res) => {
-    const books = await Book.findAll({
-        limit: 5
+    const books = await Book.findAll({});
+    const page = (req.query.page) ? req.query.page : 1;
+    const pages = Math.ceil(books.length / limit);
+    const booksPg = await Book.findAll({
+        limit,
+        offset: 0 + (5 * (page - 1)),
     });
-    console.log(books.length);
-    res.render('index', {books});
+    res.render('index', {booksPg, pages});
 }));
 
 /* POST books search route */
 router.post('/books', asyncHandler(async (req, res) => {
     const query = req.body.query.toLowerCase();
+    const page = (req.query.page) ? req.query.page : 1;
     const books = await Book.findAll({
-        limit: 5,
         where: {
             [Op.or]: [
-                {
-                    title: {
-                    [Op.like]: `%${query}%`
-                    }
-                },
-                {
-                    author: {
-                    [Op.like]: `%${query}%`
-                    }
-                },
-                {
-                    genre: {
-                        [Op.like]: `%${query}%`
-                    }
-                },
-                {
-                    year: {
-                        [Op.like]: `%${query}%`
-                    }
-                },
+                {title: {[Op.like]: `%${query}%`}},
+                {author: {[Op.like]: `%${query}%`}},
+                {genre: {[Op.like]: `%${query}%`}},
+                {year: {[Op.like]: `%${query}%`}},
             ]
         }
     });
-    res.render('index', {books});
-}))
+    const pages = Math.ceil(books.length / limit);
+    const booksPg = await Book.findAll({
+        limit: 5,
+        offset: 0 + (5 * (page - 1)),
+        where: {
+            [Op.or]: [
+                {title: {[Op.like]: `%${query}%`}},
+                {author: {[Op.like]: `%${query}%`}},
+                {genre: {[Op.like]: `%${query}%`}},
+                {year: {[Op.like]: `%${query}%`}},
+            ]
+        }
+    });
+    res.render('index', {booksPg, pages});
+}));
 
 /* GET create New Book route */
 router.get('/books/new', (req, res) => {
@@ -86,7 +87,7 @@ router.post('/books/new', asyncHandler(async (req, res) => {
 }));
 
 /* GET book detail form */
-router.get('/books/:id', asyncHandler(async (req,res) => {
+router.get('/book/:id', asyncHandler(async (req,res) => {
     const book = await Book.findByPk(req.params.id);
     if(book) {
         res.render('update-book', {book});
